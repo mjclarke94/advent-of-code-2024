04ae9c8569dd126c9794d5be01bc014a820cc133
Matt Clarke <mattjclarke94@gmail.com>
2024-12-02 21:08:45 +0000

    Improve day 1 performance

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -18,7 +18,9 @@ fn parse_to_vec(input: &str) -> nom::IResult<&str, (Vec<u32>, Vec<u32>)> {
     })(input)
 }
 
-fn parse_to_counter(input: &str) -> nom::IResult<&str, (Counter<u32, u32>, Counter<u32, u32>)> {
+type UCounter = Counter<u32, u32>;
+
+fn parse_to_counter(input: &str) -> nom::IResult<&str, (UCounter, UCounter)> {
     map(separated_list1(newline, parse_line), |v| {
         v.into_iter().unzip()
     })(input)
@@ -38,18 +40,8 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let (col1, col2) = parse_to_counter(input).unwrap().1;
 
-    // let (col1, col2): (Counter<_>, Counter<_>) = input
-    //     .lines()
-    //     .filter_map(|line| {
-    //         // Split each line, parse into i32, and collect exactly two numbers
-    //         let mut nums = line
-    //             .split_whitespace()
-    //             .filter_map(|x| x.parse::<usize>().ok());
-    //         Some((nums.next()?, nums.next()?)) // Extract exactly two numbers
-    //     })
-    //     .unzip();
-
     let a: u32 = col1.into_iter().map(|f| f.0 * f.1 * col2[&f.0]).sum();
+
     Some(a)
 }
 
