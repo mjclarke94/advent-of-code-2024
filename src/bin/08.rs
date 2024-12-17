9b2be9cef3ff0ab346e9f19a5cb7d2922d390da0
Matt Clarke <mattjclarke94@gmail.com>
2024-12-17 19:44:23 +0000

    Day two plus benchmark

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,6 +1,5 @@
 advent_of_code::solution!(8);
 use itertools::Itertools;
-use std::collections::HashSet;
 use std::{ops::Range, str::FromStr};
 
 type S = i16;
@@ -9,7 +8,6 @@ struct Antenna {
     x: S,
     y: S,
     variant: char,
-    anti: bool,
 }
 
 impl Antenna {
@@ -22,15 +20,44 @@ impl Antenna {
                 x: other.x + offset_x,
                 y: other.y + offset_y,
                 variant: self.variant,
-                anti: true,
             },
             Self {
                 x: self.x - offset_x,
                 y: self.y - offset_y,
                 variant: self.variant,
-                anti: true,
             },
         ]
+    }
+
+    fn antinode_array(self, other: Self, x_domain: &Range<S>, y_domain: &Range<S>) -> Vec<Self> {
+        let offset_x = other.x - self.x;
+        let offset_y = other.y - self.y;
+
+        let oth_x = (x_domain.end - other.x.max(self.x)) / offset_x;
+        let oth_y = (y_domain.end - other.y.max(self.y)) / offset_y;
+
+        let oth_n = oth_x.max(oth_y);
+
+        let self_x = self.x.min(other.x) / offset_x;
+        let self_y = self.y.min(other.y) / offset_y;
+
+        let self_n = self_x.max(self_y);
+
+        let mut s: Vec<Self> = (0..=self_n)
+            .map(|n| Self {
+                x: self.x - n * offset_x,
+                y: self.y - n * offset_y,
+                variant: self.variant,
+            })
+            .collect();
+
+        s.extend((0..=oth_n).map(|n| Self {
+            x: other.x + n * offset_x,
+            y: other.y + n * offset_y,
+            variant: self.variant,
+        }));
+
+        s
     }
 }
 #[derive(Debug)]
@@ -50,6 +77,22 @@ impl Field {
                 group
                     .combinations(2)
                     .flat_map(|f| ((f[0]).antinode_pair(*f[1])))
+                    .filter(|f| self.validate_node(f)),
+            );
+        }
+
+        antinodes
+    }
+
+    fn get_all_antinodes(&mut self) -> Vec<Antenna> {
+        let mut antinodes: Vec<Antenna> = Vec::new();
+
+        self.antennae.sort_by_key(|a| a.variant);
+        for (_variant, group) in &self.antennae.iter().chunk_by(|a| a.variant) {
+            antinodes.extend(
+                group
+                    .combinations(2)
+                    .flat_map(|f| ((f[0]).antinode_array(*f[1], &self.x_domain, &self.y_domain)))
                     .filter(|f| self.validate_node(f)),
             );
         }
@@ -77,7 +120,6 @@ impl FromStr for Field {
                 x: i as S % w,
                 y: i as S / w,
                 variant: c,
-                anti: false,
             })
             .collect();
 
@@ -101,8 +143,16 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(an.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut f = Field::from_str(input).unwrap();
+
+    let mut an = f.get_all_antinodes();
+
+    an.sort_by_key(|f| (f.y, f.x));
+
+    an.dedup_by_key(|f| (f.y, f.x));
+
+    Some(an.len())
 }
 
 #[cfg(test)]
@@ -118,6 +168,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
